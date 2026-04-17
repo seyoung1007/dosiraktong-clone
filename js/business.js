b8d0950e6dc2e4157fbc1ff16a5b4d300c3d541b
seyoung1007 <bobb7771@gmail.com>
2026-04-17 11:50:31 +0900

    도시락통 클론코딩(반응형 제작 및 aos 제작)

---
+++ js/business.js
@@ -0,0 +1,17 @@
+window.addEventListener("load", () => {
+  const swBusiness = new Swiper(".sw-business", {
+    slidesPerView: 1,
+    // slide 간의 간격( gap)
+    // spaceBetween:20,
+    breakpoints: {
+      // 640 ~ 1023
+      480: {
+        slidesPerView: 2,
+      },
+      // 1024px 이후
+      1024: {
+        slidesPerView: 3,
+      },
+    },
+  });
+});
--- js/header.js
+++ js/header.js
@@ -23,35 +23,37 @@ window.addEventListener("load", () => {
       mbt.classList.remove("active");
     }
   });
+
   // 모바일 메뉴 클릭시
   const htmlRoot = document.querySelector("html");
   mbt.addEventListener("click", () => {
-    // contains() 현재 ani클레스가 있는지 없는지를 파악
+    // contains() 현재 ani클래스가 있는지 없는지를 파악
     const state = mbt.classList.contains("ani");
+    console.log(state);
+
     if (state) {
-      mbt.classList.remove("ani");
       navMb.classList.remove("active");
-      // 윈도우 스크롤바가 나타난다.
+      mbt.classList.remove("ani");
+      //  윈도우 스크롤바가 나타난다.
       htmlRoot.classList.remove("active");
       header.classList.remove("active");
       mbt.classList.remove("active");
     } else {
       mbt.classList.add("ani");
       navMb.classList.add("active");
-      // 윈도우 스크롤바가 없어진다.
+      //  윈도우 스크롤바가 없어진다.
       htmlRoot.classList.add("active");
       header.classList.add("active");
       mbt.classList.add("active");
     }
-    // console.log(state);
   });
   // 반응형 처리
   // 모바일 메뉴 초기화
-
+  // let winW = window.innerWidth;
   // console.log(winW);
   window.addEventListener("resize", () => {
     let winW = window.innerWidth;
-    // moblie ===> pc전환
+    // mobile ===> pc전환
     if (winW > 1024) {
       mbt.classList.remove("ani");
       navMb.classList.remove("active");
